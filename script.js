c177e803a08a5b8951a13aa1fcd930a04926ac6a
deluus polycarp <pdeluus@gmail.com>
2021-12-11 15:31:58 -0800

    hw6

--- script.js
+++ script.js
@@ -7,56 +7,19 @@ var resultSearchTerm =document.querySelector('#result-search-term');
 var formSubmitHandler = function (event) {
     event.preventDefault();
   
-    var username = cityNameEl.value.trim();
+    var cityName= cityNameEl.value.trim();
   
-    if (username) {
-      getUserRepos(username);
+    if (cityName) {
+      geoData(cityName);
   
       resultContainerEl.textContent = '';
       cityNameEl.value = '';
     } else {
       alert('Please enter a City name');
+      console.log(cityName)
     }
   };
 
-
-
-
-  var displayCity = function (city) {
-    if (city.length === 0) {
-      resultContainerEl.textContent = 'No cities found.';
-      return;
-    }
-
-
-    
-  for (var i=0; i < city.length; i ++){    
-    var cityName = city[i] + '/' + city;
-
-    var cityEl = document.createElement('a');
-    cityEl.classList = 'list-item flex-row justify-space-between align-center';
-    cityEl.setAttribute('href', './single-repo.html?repo=' + cityName);
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = cityName;
-
-    cityEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (city[i].city_count >= 5) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + city[i] + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    cityEl.appendChild(statusEl);
-
-    resultContainerEl.appendChild(cityEl);
-  
-};
 
 // render/ dispaly weather data to the page
 
@@ -70,23 +33,12 @@ $('.user-form').on('submit', function() {
 })
 
 // // form the <button> element, listen to the "click"
-
-
-
-
-
  //  select the <input>, get its value, and provide it to the geo API
-
-
-
 //  form the <button> container element , listen to the "click"
 $('.container').on('click','button',function(event){
 
     event.target
 });
-
-
-
 
 // fetch the Geo data (latitude and longitute)
 //    required info : Q = name of the city 
@@ -95,10 +47,10 @@ $('.container').on('click','button',function(event){
 
  function geoData(cityName) {
 
-    var apiKey = "77eaa9b7e9cd8a601a1ff0d76468db72"
-    var url = 'http://api.openweathermap.org/data/2.5/onecall?';
+    var geoApi = "77eaa9b7e9cd8a601a1ff0d76468db72"
+    var geoUrl = 'http://api.openweathermap.org/data/2.5/onecall?'+ cityName;
 
-    fetch(url)
+    fetch(geoUrl)
 
     .then(function(response){
         return response.json();
@@ -114,10 +66,13 @@ $('.container').on('click','button',function(event){
 
 // fetch the one call weather data
 function oneCall(lat, lon) {
+  var oneApi = " 77eaa9b7e9cd8a601a1ff0d76468db72"
+  var oneUrl = "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=${API Key} ";
+  
      
-    var url = "";
+    
 
-    fetch(url)
+    fetch(oneUrl)
 
     .then(function(response){
         return response.json();
@@ -127,12 +82,49 @@ function oneCall(lat, lon) {
         console.log(data)
 
         // render to page.
-        var displayWeather = function(){
+        // var displayWeather = function(){
             
-        }
+        // }
     });
 
  }
+
+
+  var displayWeather = function (city) {
+    if (city.length === 0) {
+      resultContainerEl.textContent = 'No cities found.';
+      return;
+    }
+
+
+    
+//   for (var i=0; i < city.length; i ++){    
+//     var cityName = city[i] + '/' + city;
+
+//     var cityEl = document.createElement('a');
+//     cityEl.classList = 'list-item flex-row justify-space-between align-center';
+//     cityEl.setAttribute('href', './single-repo.html?repo=' + cityName);
+
+//     var titleEl = document.createElement('span');
+//     titleEl.textContent = cityName;
+
+//     cityEl.appendChild(titleEl);
+
+//     var statusEl = document.createElement('span');
+//     statusEl.classList = 'flex-row align-center';
+
+//     if (city[i].city_count >= 5) {
+//       statusEl.innerHTML =
+//         "<i class='fas fa-times status-icon icon-danger'></i>" + city[i] + ' issue(s)';
+//     } else {
+//       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+//     }
+
+//     cityEl.appendChild(statusEl);
+
+//     resultContainerEl.appendChild(cityEl);
+  
+// };
 
 
 //    = lat
@@ -145,5 +137,5 @@ function oneCall(lat, lon) {
 
 
 //  print/ render the weather datat to the page
-// userFormEl.addEventListener('submit', formSubmitHandler)
+userFormEl.addEventListener('submit', formSubmitHandler)
 }
