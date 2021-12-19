263956c430233317d45959c85c7e761ab25d05c1
deluus polycarp <pdeluus@gmail.com>
2021-12-18 16:09:39 -0800

    hw6

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 var userFormEl = document.querySelector('#user-form');
-var cityNameEl = document.querySelector('#cityname');
+var cityNameEl = document.querySelector('#cityInput');
 var btnEl = document.querySelector('#btn')
 var resultContainerEl = document.querySelector('#result-container');
 var resultSearchTerm = document.querySelector('#result-search-term');
 var pastSearches = JSON.parse(window.localStorage.getItem('pastSearches')) || [];
+var forecastEl = document.querySelector('#forecast-container')
 
 // render/ dispaly weather data to the page
+$('#pastSearches').on('click', 'li', function() {
+  geoData($(this).text());
+});
+
+// Will create a list item element to append to our HTMLs unordered list
+function listItem(citiesHistory) {
+  var li = $('<li>').addClass('list-group-item list-group-item-action').text(citiesHistory);
+  $('#pastSearches').append(li);
+};
 
 //  form the <form> element. listen to the "submit"
 $('#user-form').on('submit', function () {
   var city = $(this).children('#cityInput').val();
   event.preventDefault()
-  if (pastSearches.indexOf(city) === -1) {
-    pastSearches.push(city);
-    window.localStorage.setItem("pastSearches", JSON.stringify(pastSearches));
-  }
 
   if (city) {
     geoData(city);
@@ -37,6 +43,12 @@ function geoData(cityName) {
 
   var geoApi = "77eaa9b7e9cd8a601a1ff0d76468db72"
   var geoUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + geoApi;
+
+  if(pastSearches.indexOf(cityName) === -1) {
+    pastSearches.push(cityName);
+    window.localStorage.setItem('pastSearches', JSON.stringify(pastSearches))
+    listItem(cityName)
+  }
 
   // getting API response and returning it with a parsing into js object 
   // THEN function calling oneCall function for API data results 
@@ -66,11 +78,41 @@ function oneCall(lat, lon) {
       return response.json();
     })
     .then(function (data) {
-      var currentDate = moment(data.current.dt, 'X').format('L')
-      var forecastDate = moment(data.daily[1].dt, 'X').format('L')
 
+      $("#result-container").empty()
+        var card = $('<div>').addClass('card').addClass('col-9')
+        var cardBody = $('<div>').addClass('card-body p-2')
+        var date = $('<div>').text(moment(data.current.dt, 'X').format('LL')).addClass('card-text');
+        //var name = $("<div>").attr("src", "https://openweathermap.org" + data);
+        var temp= $("<div>").text('Temp: '+ data.current.temp + 'F').addClass('card-text').css('paddingTop', '10px');
+        var humidity = $("<div>").text('Humidity: ' + data.current.humidity + '%').addClass('card-text').css('paddingTop', '10px');
+        var wind_speed = $("<div>").text('Wind Speed: ' + data.current.wind_speed + 'MPH').addClass('card-text').css('paddingTop', '10px');
+        var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png");
+
+        date.append(img)
+        cardBody.append(date, temp,humidity,wind_speed);
+        card.append(cardBody)
+        $("#result-container").append(card)
       console.log(data)
-      console.log(currentDate, forecastDate)
+
+
+      forecastEl.innerHTML = '';
+      for (i = 1; i < data.daily.length - 2; i++) {
+        var card = $('<div>').addClass('card').addClass('col-2 bg-primary text-white')
+        var cardBody = $('<div>').addClass('card-body p-2')
+        var date = $('<div>').text(moment(data.daily[i].dt, 'X').format('L')).addClass('card-text');
+        //var name = $("<div>").attr("src", "https://openweathermap.org" + data);
+        var temp= $("<div>").text('Temp: '+ data.daily[i].temp.max + 'F').addClass('card-text').css('paddingTop', '10px');
+        var humidity = $("<div>").text('Humidity: ' + data.daily[i].humidity + '%').addClass('card-text').css('paddingTop', '10px');
+        var wind_speed = $("<div>").text('Wind Speed: ' + data.daily[i].wind_speed + 'MPH').addClass('card-text').css('paddingTop', '10px');
+        var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.daily[i].weather[0].icon + ".png");
+        date.append(img)
+        cardBody.append(date, temp,humidity,wind_speed);
+        card.append(cardBody)
+        $('#forecast-container').append(card)
+           // Define vars and elements for forecast cards
+        //document.getElementById("div" + (i + 1) + "Min").innerHTML = "Min" + Number(data.list[i].data.current.temp_min - 280.23).toFixed(1) + "*";
+      }
 
       //Define vars for all data that you will need Temp, Humidity, WindSpeed, UVIndex and Icon
       // function dispalyWeather(data) {
@@ -87,22 +129,7 @@ function oneCall(lat, lon) {
             return;
           }
 
-        for (i = 1; i < data.daily.length - 2; i++) {
 
-        var card = $('<div>')
-        var name = $("<div>").attr("src", "https://openweathermap.org" + data);
-        var temp= $("<div>").attr("src", "https://openweathermap.org" + data.current);
-        var humidity = $("<div>").attr("src", "https://openweathermap.org" + data.current);
-        var wind_speed = $("<div>").attr("src", "https://openweathermap.org" + data.weather);
-        var uvi = $("<div>").attr("src", "https://openweathermap.org" + data.current);
-        var img = $("<img>").attr("src", "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
-
-       resultContainerEl.append(img)
-       resultContainerEl.append(name, temp,humidity,wind_speed,uvi);
-
-           // Define vars and elements for forecast cards
-           document.getElementById("div" + (i + 1) + "Min").innerHTML = "Min" + Number(data.list[i].data.current.temp_min - 280.23).toFixed(1) + "*";
-          }
           for (i = 1; i < data.daily.length - 2; i++) {
             // Define vars and elements for forecast cards
             document.getElementById("div" + (i + 1) + "Max").innerHTML = "Max" + Number(data.list[i].data.current.temp_max - 280.23).toFixed(1) + "*";
@@ -143,3 +170,7 @@ function oneCall(lat, lon) {
   //  print/ render the weather datat to the page
   userFormEl.addEventListener('submit', geoData)
 };
+
+for(var i = 0; i<pastSearches.length; i++) {
+  listItem(pastSearches[i]);
+}
